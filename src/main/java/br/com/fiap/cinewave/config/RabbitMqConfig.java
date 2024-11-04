4150838129630d0ee3b27c1daf0c2af1a054daa7
Alberto <albertoperuchi11@gmail.com>
2024-11-04 19:13:27 -0300

    Finalizando as configurações de recursos de mensageria

--- src/main/java/br/com/fiap/cinewave/config/RabbitMqConfig.java
+++ src/main/java/br/com/fiap/cinewave/config/RabbitMqConfig.java
@@ -1,10 +1,16 @@
 package br.com.fiap.cinewave.config;
 
 import org.springframework.amqp.core.Queue;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class RabbitMqConfig {
+
+    @Bean
+    public Queue cinewaveQueue() {
+        return new Queue("cinewave_queue", true); // true indica que a fila é durável
+    }
 
     public Queue queue(){
         return new Queue("email-queue", true);
